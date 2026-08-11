d579adffea0cbcdf53f85176931df4383109a900
Mann <mansharma545@gmail.com>
2026-08-11 10:42:01 +0530

    Improve responsive hero section and GSAP animations

--- components/Team/data.ts
+++ components/Team/data.ts
@@ -4,14 +4,14 @@ export const teamMembers: TeamMember[] = [
     {
         initial: "P",
         name: "Mr. Pratham Mittal",
-        role: "Director",
+        role: "",
         description:
             "",
     },
     {
         initial: "A",
         name: "Mr. Ashish Choudhary",
-        role: "Director",
+        role: "",
         description:
             "",
     },
